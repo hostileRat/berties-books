3eca15b804cdaa25cdc01cfda8c96a826a675507
hostileRat <tcurt001@gold.ac.uk>
2023-11-14 15:42:26 +0000

    fixed a bug

--- routes/main.js
+++ routes/main.js
@@ -1,24 +1,40 @@
-module.exports = function(app, shopData) {
-
-    // Handle our routes
-    app.get('/',function(req,res){
-        res.render('index.ejs', shopData)
+module.exports = function (app, shopData) {
+  // Handle our routes
+  app.get("/", function (req, res) {
+    res.render("index.ejs", shopData);
+  });
+  app.get("/about", function (req, res) {
+    res.render("about.ejs", shopData);
+  });
+  app.get("/search", function (req, res) {
+    res.render("search.ejs", shopData);
+  });
+  app.get("/search-result", function (req, res) {
+    //searching in the database
+    res.send("You searched for: " + req.query.keyword);
+  });
+  app.get("/register", function (req, res) {
+    res.render("register.ejs", shopData);
+  });
+  app.post("/registered", function (req, res) {
+    // saving data in database
+    res.send(
+      " Hello " +
+        req.body.first +
+        " " +
+        req.body.last +
+        " you are now registered!  We will send an email to you at " +
+        req.body.email
+    );
+  });
+  app.get("/list", function (req, res) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+      res.send(result);
     });
-    app.get('/about',function(req,res){
-        res.render('about.ejs', shopData);
-    });
-    app.get('/search',function(req,res){
-        res.render("search.ejs", shopData);
-    });
-    app.get('/search-result', function (req, res) {
-        //searching in the database
-        res.send("You searched for: " + req.query.keyword);
-    });
-    app.get('/register', function (req,res) {
-        res.render('register.ejs', shopData);                                                                     
-    });                                                                                                 
-    app.post('/registered', function (req,res) {
-        // saving data in database
-        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-    }); 
-}
+  });
+};
